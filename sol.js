a20f3091f84b174616da42d2269bc5fe52fbb8bd
sanshao <sanshaolv@gmail.com>
2024-12-20 23:10:36 +0800

    x

--- sol.js
+++ sol.js
@@ -117,7 +117,7 @@ const getTokenInfoByPuppteer = async (ca) => {
   return null;
 };
 
-getTokenInfoByPuppteer('eL5fUxj2J4CiQsmW85k5FG9DvuQjjUoBHoQBi2Kpump')
+// getTokenInfoByPuppteer('eL5fUxj2J4CiQsmW85k5FG9DvuQjjUoBHoQBi2Kpump')
 
 const fetchTokenDataByAxios = async (ca) => {
   const url = `https://gmgn.ai/_next/data/BuKFsRDHemPLDNhH-GMOd/sol/token/${ca}.json?chain=sol&token=${ca}`;
